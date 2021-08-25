294729a9a8161db5b137d3a81b9680c243d5f330
nicohnavarro <nicohnavarro@gmail.com>
2021-08-24 23:58:19 -0300

    :fire:

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import {useEffect} from 'react';
 import './App.css';
 
+const url = 'https://randomuser.me/api?results=20'
+
+const fetchData = ()=>{
+  return axios.get(url)
+    .then((res)=>{
+    console.log(res.data);
+  })
+  .catch((err)=>{
+    console.error(err);
+  })
+}
+
 function App() {
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
