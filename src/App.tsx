d3e79e6f0cb3154f9fc80c63e868bde9afeb4480
nicohnavarro <nicohnavarro@gmail.com>
2021-08-26 07:27:46 -0300

    :fire:

--- src/App.tsx
+++ src/App.tsx
@@ -7,42 +7,120 @@ const url = "https://randomuser.me/api?results=20";
 type Person = any;
 type Location = any;
 
+enum SortingDirections {
+  ASCENCING = "ASCENDING",
+  DESCENDING = "DESCENDING",
+  UNSORTED = "UNSORTED",
+}
+
 const fetchData = () => {
   return axios
     .get(url)
     .then((res) => {
-      console.log(res.data.results);
       return res.data.results;
     })
-    .catch((err) => {
-      console.error(err);
-    });
+    .catch((err) => {});
 };
 
 const flattenLocations = (locations: Location[]): any => {
-  const location = locations[0];
+  const data: any[] = [];
 
-  const flattenedLocationHeaders:string[] = [];
-  Object.keys(location).forEach((locationKey) => {
-    const value = location[locationKey];
+  for (const { street, coordinates, timezone, ...rest } of locations) {
+    data.push({
+      ...rest,
+      number: street.number,
+      name: street.name,
+      latitude: coordinates.latitude,
+      longitude: coordinates.longitude,
+    });
+  }
+  const flattenedLocationHeaders: string[] = extractObjectKeys(data[0]);
+  return { headers: flattenedLocationHeaders, data };
+};
+
+const extractObjectKeys = (object: any) => {
+  let objectKeys: string[] = [];
+  Object.keys(object).forEach((objectKey) => {
+    const value = object[objectKey];
     if (typeof value !== "object") {
-      flattenedLocationHeaders.push(locationKey);
+      objectKeys.push(objectKey);
+    } else {
+      objectKeys = [...objectKeys, ...extractObjectKeys(value)];
     }
   });
-  console.log(flattenedLocationHeaders);
+  return objectKeys;
 };
+
+const sortData = (
+  data: any,
+  sortKey: string,
+  sortingDirection: SortingDirections
+) => {
+  data.sort((a: any, b: any) => {
+    const relevantValueA = a[sortKey];
+    const relevantValueB = b[sortKey];
+    if (
+      sortingDirection === SortingDirections.UNSORTED ||
+      sortingDirection === SortingDirections.ASCENCING
+    ) {
+      if (relevantValueA < relevantValueB) return -1;
+      if (relevantValueA > relevantValueB) return 1;
+      return 0;
+    } else {
+      if (relevantValueA > relevantValueB) return -1;
+      if (relevantValueA < relevantValueB) return 1;
+      return 0;
+    }
+  });
+};
+
+const getNextSortingDirection =(current:SortingDirections)=>{
+  if (
+    current === SortingDirections.UNSORTED ||
+    current === SortingDirections.ASCENCING
+  ) return SortingDirections.DESCENDING;
+   else return SortingDirections.ASCENCING;
+}
 
 function App() {
   const [people, setPeople] = useState([]);
-  const [flattenedLocations, setFlattenedLocations] = useState([]);
+  const [flattenedLocations, setFlattenedLocations] = useState({
+    headers: [],
+    data: [],
+  });
+  const [sortingDirections, setSortingDirections] = useState<any>({});
+
   useEffect(() => {
     fetchData().then((apiPeople) => {
       setPeople(apiPeople);
-      setFlattenedLocations(
-        flattenLocations(apiPeople.map(({ location }: Location) => location))
+      //console.log(extractObjectKeys(apiPeople[0]));
+      const ourFlattenedLocations = flattenLocations(
+        apiPeople.map(({ location }: Location) => location)
       );
+      setFlattenedLocations(ourFlattenedLocations);
+      const { headers } = ourFlattenedLocations;
+      const ourSortingDirections: any = {};
+      for (const header of headers) {
+        ourSortingDirections[header] = SortingDirections.UNSORTED;
+      }
+      setSortingDirections(ourSortingDirections);
     });
   }, []);
+
+  const sortColumn = (sortKey: string) => {
+    const newFlattenedLocations = {
+      ...flattenedLocations,
+      data: [...flattenedLocations.data],
+    };
+
+    const currentSortingDirection = sortingDirections[sortKey];
+    sortData(newFlattenedLocations.data,sortKey,currentSortingDirection);
+    const nextSortingDirection = getNextSortingDirection(currentSortingDirection);
+    const newSortingDirections:any = {...sortingDirections};
+    newSortingDirections[sortKey] = nextSortingDirection;
+    setSortingDirections(newSortingDirections);
+    setFlattenedLocations(newFlattenedLocations);
+  };
 
   return (
     <div className="App">
@@ -50,6 +128,40 @@ function App() {
       {people.map((person: Person, personId: number) => (
         <div key={personId}>{person.name.first}</div>
       ))}
+      <table>
+        <thead>
+          <tr>
+            {flattenedLocations.headers.map(
+              (locationString: string, locationIdx: number) => (
+                <th
+                  onClick={() => {
+                    sortColumn(locationString);
+                  }}
+                  key={locationIdx}
+                >
+                  {locationString}
+                </th>
+              )
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {flattenedLocations.data.map(
+            (location: any, locationsIdx: number) => (
+              <tr key={locationsIdx}>
+                {/*                 {Object.values(location).map(
+                  (locationValue: any, valueIdx: number) => (
+                    <td key={valueIdx}>{locationValue}</td>
+                  )
+                )} */}
+                {flattenedLocations.headers.map((header, headerIdx) => (
+                  <td key={headerIdx}>{location[header]}</td>
+                ))}
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
