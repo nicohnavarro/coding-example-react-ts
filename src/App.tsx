7c5cc12a6a8fec6229852b9a7ed5846d3080da47
nicohnavarro <nicohnavarro@gmail.com>
2021-08-25 23:29:45 -0300

    :fire:

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,55 @@
-import axios from 'axios';
-import {useEffect} from 'react';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 
-const url = 'https://randomuser.me/api?results=20'
+const url = "https://randomuser.me/api?results=20";
 
-const fetchData = ()=>{
-  return axios.get(url)
-    .then((res)=>{
-    console.log(res.data);
-  })
-  .catch((err)=>{
-    console.error(err);
-  })
-}
+type Person = any;
+type Location = any;
+
+const fetchData = () => {
+  return axios
+    .get(url)
+    .then((res) => {
+      console.log(res.data.results);
+      return res.data.results;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+const flattenLocations = (locations: Location[]): any => {
+  const location = locations[0];
+
+  const flattenedLocationHeaders:string[] = [];
+  Object.keys(location).forEach((locationKey) => {
+    const value = location[locationKey];
+    if (typeof value !== "object") {
+      flattenedLocationHeaders.push(locationKey);
+    }
+  });
+  console.log(flattenedLocationHeaders);
+};
 
 function App() {
-
-  useEffect(()=>{
-    fetchData();
-  },[])
-
+  const [people, setPeople] = useState([]);
+  const [flattenedLocations, setFlattenedLocations] = useState([]);
+  useEffect(() => {
+    fetchData().then((apiPeople) => {
+      setPeople(apiPeople);
+      setFlattenedLocations(
+        flattenLocations(apiPeople.map(({ location }: Location) => location))
+      );
+    });
+  }, []);
 
   return (
     <div className="App">
+      <h2>Challenge</h2>
+      {people.map((person: Person, personId: number) => (
+        <div key={personId}>{person.name.first}</div>
+      ))}
     </div>
   );
 }
